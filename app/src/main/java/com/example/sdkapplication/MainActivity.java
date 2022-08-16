3e289c93c7a21eca366b91dfe8e384ca2e3ae711
ManojIDPal <96538553+ManojIDPal@users.noreply.github.com>
2022-08-16 18:07:23 +0530

    Updated text in activity

--- app/src/main/java/com/example/sdkapplication/MainActivity.java
+++ app/src/main/java/com/example/sdkapplication/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.sdkapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.tv_center);
+        textView.setText("Testing Sample SDK");
     }
 }
